048ae0593beb27f8fa97f22f10d791c8a858ab57
Tom Saunders <tsuanders83@gmail.com>
2022-08-17 16:34:16 +0100

    Adding loading spinner

--- frontend/resources/js/components/Loading.tsx
+++ frontend/resources/js/components/Loading.tsx
@@ -1,11 +1,29 @@
 import { h } from 'preact';
+import styled from 'styled-components';
+import Spinner from './Icons/Spinner';
 
 /** @jsx h */
 
-const Loading = () => {
-    return (
-    <div>Loading...</div>
-  );
-};
+const StyledDiv = styled.div`
+  width: 32px;
+  height: 32px;
+  margin: 0px auto;
+  @keyframes rotation {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(359deg);
+    }
+  }
+  animation: rotation 2s infinite linear; 
+}
+`;
+
+const Loading = () => (
+  <StyledDiv>
+    <Spinner />
+  </StyledDiv>
+);
 
 export default Loading;
