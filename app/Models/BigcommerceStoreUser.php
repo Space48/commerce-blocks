7ba97d3e891a52c78cc48744f952cb0edfed3d56
Tom Robertshaw <tom.robertshaw@space48.com>
2022-08-20 21:10:21 +0100

    Add phpdoc to models

--- app/Models/BigcommerceStoreUser.php
+++ app/Models/BigcommerceStoreUser.php
@@ -5,6 +5,30 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
+/**
+ * App\Models\BigcommerceStoreUser
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $bigcommerce_store_id
+ * @property bool $is_owner
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\BigcommerceStore $store
+ * @property-read \App\Models\User $user
+ * @method static \Database\Factories\BigcommerceStoreUserFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|BigcommerceStoreUser newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BigcommerceStoreUser newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BigcommerceStoreUser owner()
+ * @method static \Illuminate\Database\Eloquent\Builder|BigcommerceStoreUser query()
+ * @method static \Illuminate\Database\Eloquent\Builder|BigcommerceStoreUser whereBigcommerceStoreId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BigcommerceStoreUser whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BigcommerceStoreUser whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BigcommerceStoreUser whereIsOwner($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BigcommerceStoreUser whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BigcommerceStoreUser whereUserId($value)
+ * @mixin \Eloquent
+ */
 class BigcommerceStoreUser extends Pivot
 {
     use HasFactory;
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,38 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string|null $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int|null $bigcommerce_id
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\BigcommerceStore[] $stores
+ * @property-read int|null $stores_count
+ * @method static \Database\Factories\UserFactory factory(...$parameters)
+ * @method static Builder|User newModelQuery()
+ * @method static Builder|User newQuery()
+ * @method static Builder|User query()
+ * @method static Builder|User whereBigcommerceId($value)
+ * @method static Builder|User whereCreatedAt($value)
+ * @method static Builder|User whereEmail($value)
+ * @method static Builder|User whereEmailVerifiedAt($value)
+ * @method static Builder|User whereId($value)
+ * @method static Builder|User whereName($value)
+ * @method static Builder|User wherePassword($value)
+ * @method static Builder|User whereRememberToken($value)
+ * @method static Builder|User whereUpdatedAt($value)
+ * @method static Builder|User withEmail(string $email)
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
