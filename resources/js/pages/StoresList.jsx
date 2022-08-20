367aa2ed8e40c5fe9fc0743142cdb65ec0ecd296
Tom Robertshaw <tom.robertshaw@space48.com>
2022-08-20 21:11:20 +0100

    Add in store switcher

---
+++ resources/js/pages/StoresList.jsx
@@ -0,0 +1,51 @@
+import React, {useContext} from 'react';
+import {Button, Flex, FlexItem, H2, H3, Panel, Text} from '@bigcommerce/big-design';
+import {PageBody, PageHeader} from '../components';
+import ConfigContext from '../context/ConfigContext';
+import {Link as RouterLink} from 'react-router-dom';
+import {useStores} from '../hooks';
+import ContentLoading from '../components/ContentLoading';
+
+const StoresList = () => {
+  const context = useContext(ConfigContext);
+  const [storesResponse, error, isLoading] = useStores();
+  const stores = storesResponse?.data;
+
+  return (
+    <>
+      <PageHeader title={context.appName}>
+        <Text>{context.appDescription}</Text>
+      </PageHeader>
+      <PageBody>
+        <ContentLoading
+          loading={isLoading}
+          error={(error !== undefined) ? 'Unable to load stores' : ''}
+        >
+          <H2>Choose a store</H2>
+          {
+            stores && stores.map(store => (
+              <RouterLink key={store.id} to={`/stores/${store.store_hash}`} style={{textDecoration: 'none'}}>
+                <Panel marginBottom="medium">
+                  <Flex justifyContent="space-between">
+                    <FlexItem>
+                      <H3>{store.name}</H3>
+                    </FlexItem>
+                    <FlexItem>
+                      <Button variant="secondary">View</Button>
+                    </FlexItem>
+                  </Flex>
+                </Panel>
+              </RouterLink>
+            ))
+          }
+          {
+            !stores &&
+            <H2>No stores found</H2>
+          }
+        </ContentLoading>
+      </PageBody>
+    </>
+  );
+}
+
+export default StoresList;
