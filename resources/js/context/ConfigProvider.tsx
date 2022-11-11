c6e3a59201d08b6779ab8e04f554ca78b6c5b580
Tom Robertshaw <tom.robertshaw@space48.com>
2022-11-11 09:42:42 +0000

    Fix support for more config context variables.

--- resources/js/context/ConfigProvider.tsx
+++ resources/js/context/ConfigProvider.tsx
@@ -7,20 +7,33 @@ interface Props {
 }
 
 const ConfigProvider = ({children}: Props) => {
-  // .content here flags in TS
-  const appId = document.head.querySelector('meta[name="app-id"]').content;
-  const csrfToken = document.head.querySelector('meta[name="csrf-token"]').content;
+  const appId = document.head.querySelector('meta[name="app-id"]')?.content;
+  const csrfToken = document.head.querySelector('meta[name="csrf-token"]')?.content;
+  const helpscoutBeaconId = document.head.querySelector('meta[name="helpscout-beacon-id"]')?.content;
+  const noticeableToken = document.head.querySelector('meta[name="noticeable-token"]')?.content;
+  const noticeableProjectId = document.head.querySelector('meta[name="noticeable-project-id"]')?.content;
+  const noticeableLabelId = document.head.querySelector('meta[name="noticeable-label-id"]')?.content;
+  const supportDocsUrl = document.head.querySelector('meta[name="support-docs-url"]')?.content;
+  const supportDocsFAQUrl = document.head.querySelector('meta[name="support-docs-faq-url"]')?.content;
+  const supportEmail = document.head.querySelector('meta[name="support-email"]')?.content;
 
   return (
     <ConfigContext.Provider value={{
       appId,
       appName: 'Commerce Blocks',
-      appDescription: 'Sell your products on any website',
+      appDescription: 'Create a browsable shopping experience on any website',
       csrfToken,
+      helpscoutBeaconId,
+      noticeableToken,
+      noticeableProjectId,
+      noticeableLabelId,
+      supportDocsUrl,
+      supportDocsFAQUrl,
+      supportEmail,
     }}>
       {children}
     </ConfigContext.Provider>
   );
-};
+}
 
 export default ConfigProvider;
